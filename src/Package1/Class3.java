7670fab65fa154977659314e2804f9ba277bfbe8
hussain-si <hussain.psum@gmail.com>
2024-11-24 08:11:18 +0530

    Encapsulation

---
+++ src/Package1/Class3.java
@@ -0,0 +1,11 @@
+package Package1;
+
+import static Package1.Class1.*;
+
+
+public class Class3 {
+    public static void main(String[] args) {
+        demo1();
+        demo2();
+    }
+}
---
+++ src/pacage2/class2.java
@@ -0,0 +1,12 @@
+package pacage2;
+
+import Package1.Class1;
+
+import static Package1.Class1.*;
+
+public class class2  {
+
+    public static void main(String[] args) {
+        demo2();
+    }
+}
